134a75751cfc7833c1a19814ebdf1276c5cc9e36
Shabda Raaj <shabda.raaj@gmail.com>
2011-02-06 19:25:08 +0530

    Initial commit

---
+++ Classes/TaxCalcPhoneGapAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  TaxCalcPhoneGapAppDelegate.h
+//  TaxCalcPhoneGap
+//
+//  Created by Shabda Raaj on 06/02/11.
+//  Copyright Agiliq 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface TaxCalcPhoneGapAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TaxCalcPhoneGap
+//
+//  Created by Shabda Raaj on 06/02/11.
+//  Copyright Agiliq 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"TaxCalcPhoneGapAppDelegate");
+    [pool release];
+    return retVal;
+}
